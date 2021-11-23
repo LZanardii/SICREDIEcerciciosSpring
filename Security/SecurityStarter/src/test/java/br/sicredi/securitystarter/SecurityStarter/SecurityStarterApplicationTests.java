eed0b6fefbcc6a35006e25143efa7a3a99e8fe69
Leonardo Zanardi <leonardo_zanardi@sicredi.com.br>
2021-11-23 16:58:07 -0300

    security v0.1 com bug

---
+++ Security/SecurityStarter/src/test/java/br/sicredi/securitystarter/SecurityStarter/SecurityStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.sicredi.securitystarter.SecurityStarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
